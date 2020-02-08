2478406586e10576df31840a0ab19280e6b9773a
YongKunWang <wang_yongkun@yeah.net>
2020-02-08 10:55:59 +0800

    Java核心编程第四章对象和类

---
+++ Java_03_对象和类/sources/ExampleSingle1.java
@@ -0,0 +1,35 @@
+//ExampleSingle.java
+/**
+ * 单例设计模式之懒汉式
+ * @data:2020年2月4日
+ */
+class Single {
+    private Single (){}
+    private static Single single  = null;
+    private int num = 0;
+    public static Single getInstance(){
+        if(single == null)
+            single = new Single();
+        return single;
+    }
+    public void setNum(int num){
+        this.num = num;
+    }
+    public int getNum(){
+        return this.num;
+    }
+}
+
+public class ExampleSingle1{
+
+    public static void main(String[] args)
+    {
+        Single single = Single.getInstance();
+        System.out.println(single.getNum()); // 0
+        single.setNum(10);
+        System.out.println(single.getNum()); //10
+
+        Single single1 = Single.getInstance();
+        System.out.println(single1.getNum()); //10
+    }
+}
